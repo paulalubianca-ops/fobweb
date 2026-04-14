3f6584f4601129b4dc6a6cf652233dbff0359852
Paula Lubianca <paulalubianca@gmail.com>
2026-04-14 21:37:34 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import HeroSection from '../components/home/HeroSection';
 import ProblemSection from '../components/home/ProblemSection';
 import WhoWeAre from '../components/home/WhoWeAre';
 import TakePart from '../components/home/TakePart';
-import EventsSpotlight from '../components/home/EventsSpotlight';
 import HowItWorks from '../components/home/HowItWorks';
 import SocialProof from '../components/home/SocialProof';
 import ReviewsSection from '../components/shared/ReviewsSection';
@@ -17,7 +16,6 @@ export default function Home() {
       <ProblemSection />
       <WhoWeAre />
       <TakePart />
-      <EventsSpotlight />
       <HowItWorks />
       <SocialProof />
       <PhotoGallery variant="home" />
